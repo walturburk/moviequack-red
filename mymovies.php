dd0743e6b6035876d3623ef8375833e97c8817a0
walturburk <hietanen@gmail.com>
2018-04-15 00:05:31 +0200

    sorting streams

--- mymovies.php
+++ mymovies.php
@@ -18,27 +18,36 @@ massUpdateStreams($moviesarray, 48);
 
 $movies = getStreamableMovies($user, "bookmark");
 
+
+
 foreach ($movies AS $movie) {
   $streamsites[$movie["type"]][$movie["provider"]]["clear"] = $movie["clear"];
-  $streamsites[$movie["type"]][$movie["provider"]]["count"] = 0+$streamsites[$movie["provider"]]["count"]+1;
+  $streamsites[$movie["type"]][$movie["provider"]]["count"] = 0+$streamsites[$movie["type"]][$movie["provider"]]["count"]+1;
   $streamsites[$movie["type"]][$movie["provider"]]["movie"][] = $movie;
 
 }
 
-foreach ($streamsites AS $s) {
-  usort($s, function($a, $b) {
-      return $b['count'] <=> $a['count'];
-  });
-}
 
 $ss["Subscription"] = $streamsites["flatrate"];
-$ss["Rent"] = $streamsites["rent"];
 $ss["Buy"] = $streamsites["buy"];
+$ss["Rent"] = $streamsites["rent"];
+
+usort($ss["Subscription"], function($a, $b) {
+    return $b['count'] <=> $a['count'];
+});
+
+usort($ss["Rent"], function($a, $b) {
+    return $b['count'] <=> $a['count'];
+});
+
+usort($ss["Buy"], function($a, $b) {
+    return $b['count'] <=> $a['count'];
+});
 
 foreach ($ss AS $key => $streamsite) {
 $print .= "<p>".$key."</p>";
   foreach ($streamsite AS $s) {
-    $print .= "<h3>".$s["clear"]."</h3>";
+    $print .= "<h3>".$s["clear"]." (".$s["count"].")</h3>";
     foreach ($s["movie"] AS $movie) {
       $print .= "<a class='poster postersmall' href='movie.php?id=".$movie["movieid"]."'><img src='".basethumburl.$movie["poster"]."'/></a>";
     }
