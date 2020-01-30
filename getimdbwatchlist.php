cfa7430773e706c22f7082a9b661e2e851f3bb5e
unknown <hietanen@gmail.com>
2020-01-30 02:23:08 +0100

    asd

--- getimdbwatchlist.php
+++ getimdbwatchlist.php
@@ -6,21 +6,9 @@ $url = "https://www.imdb.com/user/ur4517943/watchlist";
 
 $html = file_get_contents($url);
 
-$metatags = get_meta_tags($url);
-
-$doc = new DOMDocument();
-@$doc->loadHTML($html);
-
-$searchNodes = $doc->getElementsByTagName( "meta" );
-
-foreach( $searchNodes as $searchNode )
-{
-    $valueID = $searchNode->getAttribute('pageId');
-    print_r($searchNode);
-
-}
-var_dump($metatags);
-var_dump($valueID);
-
+$exploded = explode('meta property="pageId" content="', $html);
+$array = explode('"', $exploded[1]);
+echo "<h1>".$array[0]."</h1>";
+print_r($exploded);
 
 ?>
