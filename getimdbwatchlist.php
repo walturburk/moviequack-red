794cfff7d5ef66be171acf029b85471913c041ec
unknown <hietanen@gmail.com>
2020-01-30 02:11:54 +0100

    asd

--- getimdbwatchlist.php
+++ getimdbwatchlist.php
@@ -7,7 +7,12 @@ $url = "https://www.imdb.com/user/ur4517943/watchlist";
 $html = file_get_contents($url);
 
 $metatags = get_meta_tags($url);
+
+$doc = new DOMDocument();
+@$doc->loadHTML($html);
+$nodes = $doc->getElementsByTagName('meta');
 var_dump($metatags);
+var_dump($nodes);
 
 
 ?>
