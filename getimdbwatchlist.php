1e6d8b807ba6a37c5b6ee3b0133fbe1b74230eb9
walter <hietanen@gmail.com>
2020-01-30 14:21:04 +0100

    asd

--- getimdbwatchlist.php
+++ getimdbwatchlist.php
@@ -1,35 +1,40 @@
 <?php
 
 $input = "https://www.imdb.com/user/ur4517943/watchlist?ref_=uspf_ci";
-$pos = strpos($input, "ur");
-$substr = substr($input, $pos+2);
-$arr = str_split($substr);
-print_r($arr);
-$usernumbers = array("u", "r");
-foreach ($arr AS $char) {
-    echo $char;
-    if ($char >= 0) {
-        $usernumbers[] = $char;
+$pos = strpos($input, "ur"); //get pos of imdb username prefix
+$substr = substr($input, $pos+2); //get all chars after username prefix "ur"
+$arr = str_split($substr); //split those into and array of characters
+
+$usernumbers = array("u", "r"); //declare array that starts with imdbs standard username prefix
+foreach ($arr AS $char) { //loop through those characters
+    if ($char >= 0) { //if it is a number
+        $usernumbers[] = $char; //put it in the array
     } else {
-        break;
+        break; //if encounter not-a-number, stop looping
     }
 }
 
-$userid = implode("", $usernumbers);
+$userid = implode("", $usernumbers); //glue together array to get userid as a string
 //$userid = "ur4517943";
 
-$url = "https://www.imdb.com/user/".$userid."/watchlist";
-echo "url:".$url;
+$url = "https://www.imdb.com/user/".$userid."/watchlist"; //insert userid into imdb watchlist array
 
-$html = file_get_contents($url);
+$html = file_get_contents($url); //get all html from watchlist url
 
-$exploded = explode('meta property="pageId" content="', $html);
-$array = explode('"', $exploded[1]);
+$exploded = explode('meta property="pageId" content="', $html); //find meta-tag with property="pageId" which holds attribute "content" with the actual listid of the watchlist
+$array = explode('"', $exploded[1]); //split again by end of attr content closing citation " to get the containing value
+$listid = $array[0];
 
-$url2 = "https://www.imdb.com/list/".$array[0]."/export";
+$url2 = "https://www.imdb.com/list/".$listid."/export"; //this is the url for csv export of imdb lists by listid
 
-$csv = file_get_contents($url2);
+$csv = file_get_contents($url2); //get file contents of the csv
 
-print_r($csv);
+$csvarray = str_getcsv($csv);
+
+print_r($csvarray);
+
+foreach ($csvarray AS $value) {
+    echo $value."<br>"; 
+}
 
 ?>
