42c919dbf6791c419cdf45a2e995f6a288dd277c
unknown <hietanen@gmail.com>
2020-01-30 02:25:01 +0100

    asd

--- getimdbwatchlist.php
+++ getimdbwatchlist.php
@@ -8,7 +8,12 @@ $html = file_get_contents($url);
 
 $exploded = explode('meta property="pageId" content="', $html);
 $array = explode('"', $exploded[1]);
-echo "<h1>".$array[0]."</h1>";
+
+$url2 = "https://www.imdb.com/list/".$array[0]."/export";
+
+echo "<h1>".$url2."</h1>";
 print_r($exploded);
+
+print_r(file_get_contents($url2));
 
 ?>
