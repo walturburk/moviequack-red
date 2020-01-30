7875b331ef613cc609d313f992009c72c2255f24
unknown <hietanen@gmail.com>
2020-01-30 01:55:42 +0100

    asd

--- getimdbwatchlist.php
+++ getimdbwatchlist.php
@@ -1,18 +1,20 @@
 <?php
 
+echo phpinfo();
 
 $url = "https://www.imdb.com/user/ur4517943/watchlist";
 
 $html = file_get_contents($url);
-/*
-$dom = new DOMDocument();
 
+$dom = new DOMDocument();
 $dom->loadHTML($html);
 
 $xpath = new DOMXPath($dom);
-$divContent = $xpath->query('//meta[property="pageId"]');
-*/
-echo $html;
-print_r($html);
+$result = '';
+foreach($xpath->evaluate('//metaiv[@property="pageId"]/node()') as $childNode) {
+  $result .= $dom->saveHtml($childNode);
+}
+var_dump($result);
+
 
 ?>
